a319632cd613f5ca9910364b4268d180853e0c9e
Ivan Makarevych <makarevich.iv@gmail.com>
2023-07-22 13:22:01 +0300

    Refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import cls from './styles/App.module.scss'
 import { useSpin } from './logics/useSpin'
-import { SPIN_TIME_MILISECONDS } from '../shared/configs/constants'
 import { SpinActions } from '@/features/SpinActions'
 import { FilmCardAction } from '@/features/FilmCardAction'
 
@@ -17,8 +16,6 @@ function App() {
     isChangeMsgBtn,
   } = useSpin()
 
-  const timerDown = SPIN_TIME_MILISECONDS - countSpin
-
   return (
     <>
       <div className={cls.container}>
@@ -33,13 +30,10 @@ function App() {
             disabled={isDisableSpinBtn}
             label={isChangeMsgBtn ? 'spin again' : 'spin'}
           />
-          <div>
-            {timerDown} sec
-          </div>
-          <div>
-            {!movieSpin && 'Let fate take the wheel!... Have a spin.'}
-          </div>
-          <FilmCardAction movie={movieSpin} />
+          <FilmCardAction
+            movie={movieSpin}
+            countSpin={countSpin}
+          />
         </div>
       </div>
     </>
--- src/features/FilmCardAction/ui/FilmCardAction.tsx
+++ src/features/FilmCardAction/ui/FilmCardAction.tsx
@@ -1,11 +1,21 @@
 import { Movie } from "@/app/logics/types"
+import { SPIN_TIME_MILISECONDS } from "@/shared/configs/constants"
 
 interface FilmCardActionType {
-    movie: Movie | null
+    movie: Movie | null,
+    countSpin: number,
 }
 
-export const FilmCardAction = ({ movie }: FilmCardActionType) => {
+export const FilmCardAction = ({ movie, countSpin }: FilmCardActionType) => {
+    const timerDown = SPIN_TIME_MILISECONDS - countSpin
+
     return <>
+        <div>
+            {timerDown} sec
+        </div>
+        <div>
+            {!movie && 'Let fate take the wheel!... Have a spin.'}
+        </div>
         {movie && JSON.stringify(movie, null, 2)}
     </>
 }
