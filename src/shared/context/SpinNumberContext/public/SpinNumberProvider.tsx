29a348387d774aef60ddac3601112963cd9166c5
Ivan Makarevych <makarevich.iv@gmail.com>
2023-07-29 22:36:08 +0300

    .

--- src/shared/context/SpinNumberContext/public/SpinNumberProvider.tsx
+++ src/shared/context/SpinNumberContext/public/SpinNumberProvider.tsx
@@ -1,11 +1,18 @@
 import { ReactNode, useState } from "react";
-import { SpinNumberContex } from "../inner/SpinNumberContex";
+
+
 import { SPIN_TIME_SECONDS } from "@/shared/configs/constants";
+
+
+import { SpinNumberContex } from "../inner/SpinNumberContex";
+
+
 
 type Props = {
     children?: ReactNode,
 }
 
+// export const SpinNumberProvider: React.FC<Props> = ({ children }) => {
 export const SpinNumberProvider = ({ children }: Props) => {
     const [spinNumber, setSpinNumber] = useState(SPIN_TIME_SECONDS)
 
